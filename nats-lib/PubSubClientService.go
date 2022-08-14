f60937c3c0832be4592471db485844210535192b
kripanshdevtron <kripansh@devtron.ai>
2022-08-14 17:16:50 +0530

    workQueue deliver policy conf

--- nats-lib/PubSubClientService.go
+++ nats-lib/PubSubClientService.go
@@ -37,7 +37,8 @@ func (impl PubSubClientServiceImpl) Publish(topic string, msg string) error {
 	jetStrCtxt := natsClient.JetStrCtxt
 	natsTopic := GetNatsTopic(topic)
 	streamName := natsTopic.streamName
-	_ = AddStream(jetStrCtxt, natsClient.streamConfig, streamName)
+	streamConfig := natsClient.streamConfig
+	_ = AddStream(jetStrCtxt, streamConfig, streamName)
 	//Generate random string for passing as Header Id in message
 	randString := "MsgHeaderId-" + utils.Generate(10)
 	_, err := jetStrCtxt.Publish(topic, []byte(msg), nats.MsgId(randString))
@@ -54,12 +55,18 @@ func (impl PubSubClientServiceImpl) Subscribe(topic string, callback func(msg *P
 	streamName := natsTopic.streamName
 	queueName := natsTopic.queueName
 	consumerName := natsTopic.consumerName
-	_ = AddStream(impl.natsClient.JetStrCtxt, impl.natsClient.streamConfig, streamName)
-	_, err := impl.natsClient.JetStrCtxt.QueueSubscribe(topic, queueName, func(msg *nats.Msg) {
+	natsClient := impl.natsClient
+	streamConfig := natsClient.streamConfig
+	_ = AddStream(natsClient.JetStrCtxt, streamConfig, streamName)
+	deliveryOption := nats.DeliverLast()
+	if streamConfig.Retention == nats.WorkQueuePolicy {
+		deliveryOption = nats.DeliverAll()
+	}
+	_, err := natsClient.JetStrCtxt.QueueSubscribe(topic, queueName, func(msg *nats.Msg) {
 		defer msg.Ack()
 		subMsg := &PubSubMsg{Data: string(msg.Data)}
 		callback(subMsg)
-	}, nats.Durable(consumerName), nats.DeliverAll(), nats.ManualAck(), nats.BindStream(streamName))
+	}, nats.Durable(consumerName), deliveryOption, nats.ManualAck(), nats.BindStream(streamName))
 	if err != nil {
 		impl.logger.Fatalw("error while subscribing", "stream", streamName, "topic", topic, "error", err)
 		return err
