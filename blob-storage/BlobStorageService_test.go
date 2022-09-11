543a2b7c9da70c9393056641788254ba583b216c
kripanshdevtron <kripansh@devtron.ai>
2022-09-11 18:45:12 +0530

    put command fix

--- blob-storage/BlobStorageService_test.go
+++ blob-storage/BlobStorageService_test.go
@@ -8,7 +8,7 @@ import (
 func TestBlobStorageService(t *testing.T) {
 
 	t.SkipNow()
-	t.Run("command exec", func(t *testing.T) {
+	t.Run("Azure command exec", func(t *testing.T) {
 		var azureBlobBaseConfig *AzureBlobBaseConfig
 		azureBlobBaseConfig = &AzureBlobBaseConfig{
 			AccountKey:        "",
@@ -25,6 +25,24 @@ func TestBlobStorageService(t *testing.T) {
 		}
 		blobStorageServiceImpl := NewBlobStorageServiceImpl(nil)
 		_, _, err := blobStorageServiceImpl.Get(request)
+		fmt.Println(err)
+	})
+	t.Run("S3 command exec", func(t *testing.T) {
+		awsS3BaseConfig := &AwsS3BaseConfig{
+			AccessKey:   "ZFRSVmt2WGZQMERuRVdTZ1BjY0VjMzFxamRJPQo",
+			Passkey:     "QTdpSGY2RmR2OXQ3eVRod1Azcllhbzk3a0U4PQo",
+			EndpointUrl: "devtron-minio.devtroncd:9000",
+			BucketName:  "devtron-ci-log",
+			Region:      "us-east-2",
+		}
+		blobStorageServiceImpl := NewBlobStorageServiceImpl(nil)
+		request := &BlobStorageRequest{
+			StorageType:     BLOB_STORAGE_S3,
+			SourceKey:       "devtron/cd-artifacts/110/110.zip",
+			DestinationKey:  "job-artifact.zip",
+			AwsS3BaseConfig: awsS3BaseConfig,
+		}
+		err := blobStorageServiceImpl.PutWithCommand(request)
 		fmt.Println(err)
 	})
 
