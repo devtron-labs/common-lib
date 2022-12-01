2c699f58572beb5a5e5de198ca6d1bc7a5338372
Gireesh Naidu <gireesh@devtron.ai>
2022-12-01 09:36:28 +0530

    durable name fix

--- pubsub-lib/JetStreamUtil.go
+++ pubsub-lib/JetStreamUtil.go
@@ -63,7 +63,7 @@ const (
 	NEW_CI_MATERIAL_TOPIC_DURABLE       string = "NEW-CI-MATERIAL_DURABLE-1"
 	CD_SUCCESS                          string = "CD.TRIGGER"
 	CD_TRIGGER_GROUP                    string = "CD_TRIGGER_GRP1"
-	CD_TRIGGER_DURABLE                  string = "CD_TRIGGER_DURABLE1"
+	CD_TRIGGER_DURABLE                  string = "CD-TRIGGER-DURABLE1"
 	WEBHOOK_EVENT_TOPIC                 string = "WEBHOOK_EVENT"
 	WEBHOOK_EVENT_GROUP                 string = "WEBHOOK_EVENT_GRP"
 	WEBHOOK_EVENT_DURABLE               string = "WEBHOOK_EVENT_DURABLE"
