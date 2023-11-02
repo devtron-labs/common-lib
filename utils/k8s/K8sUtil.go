321afc8ac02f47ebc73775457411991aa6e10f53
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2023-11-02 11:31:10 +0530

    struct updated

--- utils/k8s/K8sUtil.go
+++ utils/k8s/K8sUtil.go
@@ -73,13 +73,21 @@ type K8sUtil struct {
 }
 
 type ClusterConfig struct {
-	ClusterName           string
-	Host                  string
-	BearerToken           string
-	InsecureSkipTLSVerify bool
-	KeyData               string
-	CertData              string
-	CAData                string
+	ClusterName                     string
+	Host                            string
+	BearerToken                     string
+	InsecureSkipTLSVerify           bool
+	KeyData                         string
+	CertData                        string
+	CAData                          string
+	ClusterId                       int
+	ProxyUrl                        string
+	ToConnectForClusterVerification bool
+	ToConnectWithSSHTunnel          bool
+	SSHTunnelUser                   string
+	SSHTunnelPassword               string
+	SSHTunnelAuthKey                string
+	SSHTunnelServerAddress          string
 }
 
 func NewK8sUtil(logger *zap.SugaredLogger, runTimeConfig *client.RuntimeConfig) *K8sUtil {
