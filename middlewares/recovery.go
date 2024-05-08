8e949b889685b6d09f69dd88dcf5a3f3caca4b8f
Laeeqdev <mo@devtron.ai>
2024-05-08 16:39:30 +0530

    added message

--- middlewares/recovery.go
+++ middlewares/recovery.go
@@ -16,7 +16,7 @@ func Recovery(next http.Handler) http.Handler {
 		defer func() {
 			err := recover()
 			if err != nil {
-				metrics.IncPanicRecoveryCount(r.Host, r.Method, r.RequestURI, fmt.Sprintf("%w", err))
+				metrics.IncPanicRecoveryCount(r.Host, r.Method, r.RequestURI, fmt.Sprintf("%v", err))
 				log.Print(constants.PanicLogIdentifier, "recovered from panic", "err", err, "stack", string(debug.Stack()))
 				jsonBody, _ := json.Marshal(map[string]string{
 					"error": "internal server error",
