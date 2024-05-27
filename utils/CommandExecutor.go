2458c90574ebaa7ba684fdd88111689319fc126d
prkhrkat <prkhrkat@gmail.com>
2024-05-27 12:49:08 +0530

    #5119 Feature: Masking of secrets in build logs
    rules.go added containing regex patterns
    scanner.go to scan stream and strings

--- utils/CommandExecutor.go
+++ utils/CommandExecutor.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"bytes"
+	"fmt"
+	"github.com/devtron-labs/common-lib/utils/secretScanner"
 	"io"
 	"os"
 	"os/exec"
@@ -16,5 +18,36 @@ func RunCommand(cmd *exec.Cmd) error {
 		return err
 	}
 	//log.Println(stdBuffer.String())
+	return nil
+}
+
+func RunCommandWithSecretMasking(cmd *exec.Cmd) error {
+
+	var outBuf bytes.Buffer
+	cmd.Stdout = &outBuf
+	cmd.Stderr = os.Stderr
+
+	// Run the command
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Command execution failed: %v\n", err)
+		os.Exit(1)
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+	// Call the function to mask secrets and print the masked output
+	maskedStream, err := secretScanner.MaskSecretsStream(&outBuf)
+	if err != nil {
+		fmt.Printf("Error masking secrets: %v\n", err)
+		os.Exit(1)
+		return err
+	}
+	_, er := buf.ReadFrom(maskedStream)
+	if er != nil {
+		fmt.Printf("Error reading from masked stream: %v\n", er)
+		os.Exit(1)
+		return err
+	}
+	fmt.Println(buf.String())
 	return nil
 }
