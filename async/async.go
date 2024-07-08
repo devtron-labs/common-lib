857b5f6c3766c2cfdc9e0914abd08d435601741a
Ash-exp <asutosh2000ad@gmail.com>
2024-07-08 16:52:36 +0530

    feat: common Execute method

--- async/async.go
+++ async/async.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 	"github.com/devtron-labs/common-lib/constants"
 	"github.com/devtron-labs/common-lib/pubsub-lib/metrics"
+	"github.com/devtron-labs/common-lib/utils/runTime"
 	"go.uber.org/zap"
 	"log"
 	"runtime/debug"
 )
 
 type Async struct {
-	logger           *zap.SugaredLogger
-	microServiceName MicroServiceName
+	logger      *zap.SugaredLogger
+	serviceName ServiceName
 }
 
-type MicroServiceName string
+type ServiceName string
 
-func (m MicroServiceName) ToString() string {
+func (m ServiceName) ToString() string {
 	return string(m)
 }
 
@@ -25,10 +26,10 @@ type RunAsyncMetaData struct {
 	Path   string
 }
 
-func NewAsync(logger *zap.SugaredLogger, microServiceName MicroServiceName) *Async {
+func NewAsync(logger *zap.SugaredLogger, serviceName ServiceName) *Async {
 	return &Async{
-		logger:           logger,
-		microServiceName: microServiceName,
+		logger:      logger,
+		serviceName: serviceName,
 	}
 }
 
@@ -50,7 +51,14 @@ func CallerPath(pathName string) NewUpdateMetaData {
 
 type NewUpdateMetaData func(*RunAsyncMetaData)
 
-func (impl *Async) Run(fn func(), metadataOpts ...NewUpdateMetaData) {
+func (impl *Async) Execute(runnable func()) {
+	impl.run(runnable,
+		CallerMethod(runTime.GetCallerFunctionName()),
+		CallerPath(fmt.Sprintf("%s:%d", runTime.GetCallerFileName(), runTime.GetCallerLineNumber())),
+	)
+}
+
+func (impl *Async) run(fn func(), metadataOpts ...NewUpdateMetaData) {
 	metaData := NewRunAsyncMetaData()
 	for _, metadataOpt := range metadataOpts {
 		// updating meta data
@@ -61,7 +69,7 @@ func (impl *Async) Run(fn func(), metadataOpts ...NewUpdateMetaData) {
 	go func() {
 		defer func() {
 			if r := recover(); r != nil {
-				metrics.IncPanicRecoveryCount("go-routine", impl.microServiceName.ToString(), metaData.Method, metaData.Path)
+				metrics.IncPanicRecoveryCount("go-routine", impl.serviceName.ToString(), metaData.Method, metaData.Path)
 				if impl.logger == nil {
 					log.Println(constants.GoRoutinePanicMsgLogPrefix, "go-routine recovered from panic", "err:", r, "stack:", string(debug.Stack()))
 				} else {
