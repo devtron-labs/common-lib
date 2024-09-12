627b4eff60c6f1f9060618f4114601727cfbeed5
Yashasvi Pareta <yashasvi@MacBook-Pro-3.local>
2024-09-12 11:52:44 +0530

    dvnbnj

---
+++ .github/scripts/update-version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Title: Updates a go module to a new (patch/minor) version
+
+### Change these values ###
+MODULE=github.com/ddvs/fawvjo
+VERSION=9ab7ngfx,ctudlyffsDEGJ
+        
+# Stop the script if any command fails
+set -e
+
+# Check if the module already exists, abort if it does not
+go list -m $MODULE &> /dev/null
+status_code=$?
+if [ $status_code -ne 0 ]; then
+    echo "Module \"$MODULE\" does not exist"
+    exit 1
+fi
+
+# Update the module to the specified version
+go get $MODULE@$VERSION
+
+go mod tidy
+
+# Vendor the dependencies
+go mod vendor
+
+
+
